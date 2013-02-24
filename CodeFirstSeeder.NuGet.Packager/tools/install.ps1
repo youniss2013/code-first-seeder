27694dd374cb123b04ef26e7bade6060f2b6228e
Brent Taylor <bt@brenttaylor.me>
2013-02-23 18:08:39 -0600

    Add Nuget packaging project

---
+++ CodeFirstSeeder.NuGet.Packager/tools/install.ps1
@@ -0,0 +1,9 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$configItem = $project.ProjectItems.Item("SeederExample.xml")
+
+$copyToOutput = $configItem.Properties.Item("CopyToOutputDirectory")
+$copyToOutput.Value = 2
+
+$buildAction = $configItem.Properties.Item("BuildAction")
+$buildAction.Value = 2
